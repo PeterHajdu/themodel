49aae9a94e5b6a28d61124be25857b3889734540
Peter Hajdu <peter.ferenc.hajdu@gmail.com>
2015-03-24 17:30:30 +0100

    set default receive timeout to three seconds

--- src/remote_model.cpp
+++ src/remote_model.cpp
@@ -16,13 +16,20 @@ int main( int argc, const char *argv[] )
     zmq::context_t context{ 1 };
     zmq::socket_t socket{ context, ZMQ_REQ };
 
+    const int timeout_in_ms{ 3000 };
+    socket.setsockopt( ZMQ_RCVTIMEO, &timeout_in_ms, sizeof( timeout_in_ms ) );
+
     const char* endpoint( argv[ 1 ] );
-    std::cout << "Using endpoint: " << endpoint << std::endl;
     socket.connect( endpoint );
 
     zmq::message_t message;
     socket.send( message );
-    socket.recv( &message );
+    if ( !socket.recv( &message ) )
+    {
+      std::cerr << "Did not receive response in " << timeout_in_ms << "ms." << std::endl;
+      exit( 1 );
+    }
+
     std::cout << std::string( static_cast< const char * >( message.data() ) );
   }
   catch ( std::exception& e )

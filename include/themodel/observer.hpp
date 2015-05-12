b19b209598a4f678e324d1ad4a4ead84a7a0c484
Peter Hajdu <peter.ferenc.hajdu@gmail.com>
2015-05-12 10:59:53 +0200

    add auto observer
     * it has default constructor
     * it has move assignment

--- include/themodel/observer.hpp
+++ include/themodel/observer.hpp
@@ -2,6 +2,7 @@
 
 #include <functional>
 #include <vector>
+#include <unordered_map>
 
 namespace the
 {
@@ -16,10 +17,87 @@ template < typename Model >
 class Observable
 {
   public:
+    using my_type = Observable< Model >;
     using observer_type = Observer< Model >;
+    class AutoObserver
+    {
+      public:
+        AutoObserver()
+          : m_observed{ nullptr }
+          , m_id( 0 )
+        {
+        }
+
+        AutoObserver( const my_type& observed, size_t id )
+          : m_observed( &observed )
+          , m_id( id )
+        {
+        }
+
+        ~AutoObserver()
+        {
+          remove_observer_if_initialized();
+        }
+
+        AutoObserver( const AutoObserver& ) = delete;
+        AutoObserver& operator=( const AutoObserver& ) = delete;
+
+        AutoObserver( AutoObserver&& other )
+          : m_observed( other.m_observed )
+          , m_id( other.m_id )
+        {
+        }
+
+        AutoObserver& operator=( AutoObserver&& other )
+        {
+          if ( this == &other )
+          {
+            return *this;
+          }
+
+          remove_observer_if_initialized();
+          m_id = other.m_id;
+          std::swap( m_observed, other.m_observed );
+          return *this;
+        }
+
+      private:
+        bool is_initialized() const
+        {
+          return m_observed != nullptr;
+        }
+
+        void remove_observer_if_initialized()
+        {
+          if ( !is_initialized() )
+          {
+            return;
+          }
+
+          m_observed->remove_auto_observer( m_id );
+          m_observed = nullptr;
+        }
+
+        const my_type* m_observed;
+        size_t m_id;
+    };
+
+    using auto_observer_type = AutoObserver;
+
+    Observable()
+      : m_auto_observers_next_index( 0u )
+    {
+    }
+
     void observe( observer_type observer ) const
     {
       m_observers.emplace_back( std::move( observer ) );
+    }
+
+    AutoObserver auto_observe( observer_type observer ) const
+    {
+      m_auto_observers.emplace( m_auto_observers_next_index++, observer );
+      return AutoObserver( *this, m_auto_observers_next_index - 1 );
     }
 
   protected:
@@ -29,10 +107,22 @@ class Observable
       {
         observer( static_cast< const Model& >( *this ) );
       }
+
+      for ( const auto& observer : m_auto_observers )
+      {
+        observer.second( static_cast< const Model& >( *this ) );
+      }
     }
 
   private:
+    void remove_auto_observer( size_t id ) const
+    {
+      m_auto_observers.erase( id );
+    }
+
     mutable std::vector< observer_type > m_observers;
+    mutable size_t m_auto_observers_next_index;
+    mutable std::unordered_map< size_t, observer_type > m_auto_observers;
 };
 
 }
--- test/test_observer.cpp
+++ test/test_observer.cpp
@@ -15,26 +15,80 @@ class Observed : public the::model::Observable< Observed >
 }
 
 
-Describe_Only( an_observer )
+Describe( an_observer )
 {
   void SetUp()
   {
     observed = std::make_unique< test::Observed >();
+    was_called_with = nullptr;
   }
 
-  It ( can_be_observed )
+  void assert_observable()
   {
-    const test::Observed* was_called_with{ nullptr };
-    observed->observe(
-        [ &was_called_with ]( const test::Observed& observed )
-        {
-          was_called_with = &observed;
-        } );
     AssertThat( was_called_with == nullptr, Equals( true ) );
     observed->notify();
     AssertThat( was_called_with, Equals( observed.get() ) );
   }
 
+  It ( can_be_observed )
+  {
+    observed->observe( observer );
+    assert_observable();
+  }
+
+  It ( has_auto_observer )
+  {
+    auto auto_observer( observed->auto_observe( observer ) );
+    assert_observable();
+    (void)auto_observer;
+  }
+
+  It ( does_not_notify_deleted_auto_observers )
+  {
+    {
+      auto auto_observer( observed->auto_observe( observer ) );
+      (void)auto_observer;
+    }
+    observed->notify();
+    AssertThat( was_called_with == nullptr, Equals( true ) );
+  }
+
+  It ( does_allow_default_auto_observer_creation )
+  {
+    test::Observed::auto_observer_type observer;
+  }
+
+  It ( does_allow_move_assignment_of_auto_observers )
+  {
+    test::Observed::auto_observer_type auto_observer;
+    auto_observer = observed->auto_observe( observer );
+    assert_observable();
+  }
+
+  It ( removes_previous_auto_observer_when_new_one_is_assigned )
+  {
+    bool was_new_observer_called{ false };
+    auto auto_observer( observed->auto_observe( observer ) );
+    auto_observer = observed->auto_observe(
+        [ &was_new_observer_called ]( const test::Observed& )
+        {
+          was_new_observer_called = true;
+        } );
+
+    observed->notify();
+    AssertThat( was_new_observer_called, Equals( true ) );
+
+    const bool was_old_observer_called{ was_called_with!=nullptr };
+    AssertThat( was_old_observer_called, Equals( false ) );
+  }
+
   std::unique_ptr< test::Observed > observed;
+  const test::Observed* was_called_with{ nullptr };
+
+  test::Observed::observer_type observer{
+          [ this ]( const test::Observed& observed )
+          {
+            was_called_with = &observed;
+          } };
 };
 
